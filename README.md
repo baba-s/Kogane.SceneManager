a69a935f48ef71c8a22ef949a46ab2447875ff06
baba-s <shotababa1206@gmail.com>
2022-11-30 20:16:00 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Kogane.SceneManager
+# Kogane Scene Manager
+
+SceneManager に関する処理をまとめたクラス
+
+## 使用例
+
+```csharp
+// 現在読み込まれているすべてのシーンを取得
+foreach ( var scene in KoganeSceneManager.GetAllScenes() )
+{
+    Debug.Log( scene.name );
+}
+
+// 指定された名前のシーンが読み込まれている場合 true
+Debug.Log( KoganeSceneManager.IsLoaded( "SampleScene" ) );
+```
---
+++ Runtime/KoganeSceneManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace Kogane
+{
+    /// <summary>
+    /// SceneManager に関する処理をまとめたクラス
+    /// </summary>
+    public static class KoganeSceneManager
+    {
+        //================================================================================
+        // 関数(static)
+        //================================================================================
+        /// <summary>
+        /// 現在読み込まれているすべてのシーンを返します
+        /// </summary>
+        public static IEnumerable<Scene> GetAllScenes()
+        {
+            var sceneCount = SceneManager.sceneCount;
+
+            for ( var i = 0; i < sceneCount; i++ )
+            {
+                yield return SceneManager.GetSceneAt( i );
+            }
+        }
+
+        /// <summary>
+        /// 指定された名前のシーンが読み込まれている場合 true を返します
+        /// </summary>
+        public static bool IsLoaded( string sceneName )
+        {
+            var sceneCount = SceneManager.sceneCount;
+
+            for ( var i = 0; i < sceneCount; i++ )
+            {
+                var scene = SceneManager.GetSceneAt( i );
+
+                if ( scene.name == sceneName && scene.isLoaded ) return true;
+            }
+
+            return false;
+        }
+    }
+}
